b8744595c5316726577f31afbd60f5a2beaef6f9
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-08-06 21:18:24 +0200

    Added support for when YouTube tab with search is opened

--- idnc-dialog.ts
+++ idnc-dialog.ts
@@ -15,6 +15,18 @@ function main(): void {
     case 'www.google':
       document.querySelectorAll<HTMLButtonElement>('button')[4].click();
       break;
+    case 'www.youtube':
+      Array.from(
+        document.querySelectorAll<HTMLButtonElement>(
+          'tp-yt-paper-button#button'
+        )
+      )
+        .filter(
+          (button: HTMLButtonElement): boolean =>
+            button.children[0].tagName === 'YT-FORMATTED-STRING'
+        )[1]
+        .click();
+      break;
     default:
       return;
   }
