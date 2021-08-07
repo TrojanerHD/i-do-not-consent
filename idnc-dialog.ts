8e6c24d29569d28e6f328b47519afc076a0ad674
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-08-08 01:14:32 +0200

    Updated to new YouTube form

--- idnc-dialog.ts
+++ idnc-dialog.ts
@@ -9,23 +9,19 @@ function main(): void {
   const hostArray: string[] = location.hostname.split('.');
   hostArray.pop();
   switch (hostArray.join('.')) {
-    case 'consent.youtube':
-      document.querySelector<HTMLButtonElement>('button').click();
-      break;
     case 'www.google':
       document.querySelectorAll<HTMLButtonElement>('button')[4].click();
       break;
     case 'www.youtube':
-      Array.from(
-        document.querySelectorAll<HTMLButtonElement>(
-          'tp-yt-paper-button#button'
-        )
-      )
-        .filter(
-          (button: HTMLButtonElement): boolean =>
-            button.children[0].tagName === 'YT-FORMATTED-STRING'
-        )[1]
-        .click();
+      (
+        Array.from(
+          document.querySelectorAll<HTMLAnchorElement>(
+            'a.yt-simple-endpoint.style-scope.ytd-button-renderer'
+          )
+        ).find((a: HTMLAnchorElement) =>
+          a.href.match(/^https:\/\/consent.youtube/)
+        ).children[0] as HTMLButtonElement
+      ).click();
       break;
     default:
       return;
